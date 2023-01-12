3846340b5b6339e09c0c0444d24072986730ff8d
selphaware-ahmadus <uahmad3013@outlook.com>
2023-01-12 17:38:59 +0000

    fix input args to fscom

--- fscom.py
+++ fscom.py
@@ -3,7 +3,7 @@ import sys
 
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
+    args = [""] if not len(sys.argv[1:]) else sys.argv[1:]
     test = args[0].upper() == "TEST"
     sc = SimCentral(test=test)
     sp = SimProcs(sc)
